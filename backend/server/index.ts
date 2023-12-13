da8570ee5ea6f227f547d3130035f8b3cbc4c857
Nishant Ghodke <iamcrazycoder@gmail.com>
2023-12-13 14:24:57 +0530

    chore(backend): scaffold backend app

---
+++ backend/server/index.ts
@@ -0,0 +1,41 @@
+import { createServer } from "http";
+import { createYoga } from "graphql-yoga";
+import { knex } from "knex";
+
+import knexConfig from "../knexfile";
+import buildSchema from "./utils/buildSchema";
+
+async function main(): Promise<void> {
+  try {
+    const PORT = 4000;
+
+    // Create knex client
+    const knexClient = knex(knexConfig.development);
+
+    // GraphQL resolver context
+    const context = {
+      knex: knexClient,
+    };
+
+    // Combine schemas and resolvers
+    const schema = await buildSchema();
+
+    // Server configuration
+    const yoga = createYoga({
+      schema,
+      context, // Set resolver context
+    });
+
+    // Create server
+    const server = createServer(yoga); // eslint-disable-line @typescript-eslint/no-misused-promises
+
+    // Start server
+    server.listen(PORT, () => {
+      console.info(`Server is running on http://localhost:${PORT}/graphql`);
+    });
+  } catch (error) {
+    console.error("Error starting server", error);
+  }
+}
+
+void main();
---
+++ backend/server/utils/buildSchema.ts
@@ -0,0 +1,51 @@
+import fs from "fs";
+import path from "path";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { stitchSchemas } from "@graphql-tools/stitch";
+import type { GraphQLSchema } from "graphql";
+
+/**
+ * Dynamically imports all resolvers and typeDefs files from under
+ * the server/entities directory
+ * @returns the built GraphQL schemas for all resolvers
+ */
+export default async function buildSchema(): Promise<
+  GraphQLSchema | undefined
+> {
+  try {
+    const entitiesPath = path.join(__dirname, "../entities");
+    const entityDirs = await fs.promises.readdir(entitiesPath);
+
+    const subschemas = [];
+
+    for (const entityDirname of entityDirs) {
+      if (entityDirname === ".DS_Store") continue;
+
+      const typeDefsPath = path.join(
+        entitiesPath,
+        entityDirname,
+        "typeDefs.ts"
+      );
+      const resolversPath = path.join(
+        entitiesPath,
+        entityDirname,
+        "resolvers.ts"
+      );
+
+      const typeDefs = await import(typeDefsPath);
+      const resolvers = await import(resolversPath);
+
+      subschemas.push({
+        schema: makeExecutableSchema({
+          typeDefs: typeDefs.default,
+          resolvers: resolvers.default,
+        }),
+      });
+    }
+
+    return stitchSchemas({ subschemas });
+  } catch (error) {
+    console.error("Error building schemas", error);
+    return undefined;
+  }
+}
