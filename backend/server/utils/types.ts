f6301cebe5598b0df2c3e09ff2034291ae035c20
Nishant Ghodke <wtfcrazycoder@gmail.com>
2023-12-14 21:26:45 +0530

    feat(backend): add generic to convert str to str[]

---
+++ backend/server/utils/types.ts
@@ -0,0 +1,7 @@
+export type StringToArrayOfStrings<T extends object> = {
+  [P in keyof T as T[P] extends string | null ? P : P]-?: T[P] extends
+    | string
+    | null
+    ? NonNullable<T[P]>[]
+    : NonNullable<T[P]>;
+};
