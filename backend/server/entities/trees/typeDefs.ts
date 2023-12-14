5de330102ce36c43b44a93ffbf1029c6069e7279
Nishant Ghodke <wtfcrazycoder@gmail.com>
2023-12-14 21:23:59 +0530

    feat(backend): add graphql schema for new queries

---
+++ backend/server/entities/trees/typeDefs.ts
@@ -0,0 +1,33 @@
+export default `
+  type Tree {
+    id: ID!
+    treeId: String
+    longitude: Float
+    latitude: Float
+    status: String
+    health: String
+    species: String
+    problems: [String]
+    address: String
+    zipcode: String
+    city: String
+    state: String
+  }
+
+  type CompressedTree {
+    id: ID!
+    longitude: Float
+    latitude: Float
+  }
+
+  input TreesFilter {
+    health: [String]
+    species: [String]
+    problems: [String]
+  }
+
+  type Query {
+    getTree(id: ID!): Tree
+    getTrees(filter: TreesFilter): [CompressedTree]
+  }
+`;
