00586a8b5e1ff4311f3265e7b0a684deef6bfa19
Nishant Ghodke <wtfcrazycoder@gmail.com>
2023-12-14 16:43:13 +0530

    refact(backend): replace dynamic headers w/ args

--- backend/scripts/ny-trees-importer.ts
+++ backend/scripts/ny-trees-importer.ts
@@ -10,24 +10,79 @@ import PostgresCSVImporter from "../modules/PostgresCSVImporter";
   const filePath = await downloader.download();
 
   // initialize CSV importer
+  const csvHeaders = [
+    "created_at",
+    "tree_id",
+    "block_id",
+    "the_geom",
+    "tree_dbh",
+    "stump_diam",
+    "curb_loc",
+    "status",
+    "health",
+    "spc_latin",
+    "spc_common",
+    "steward",
+    "guards",
+    "sidewalk",
+    "user_type",
+    "problems",
+    "root_stone",
+    "root_grate",
+    "root_other",
+    "trnk_wire",
+    "trnk_light",
+    "trnk_other",
+    "brnch_ligh",
+    "brnch_shoe",
+    "brnch_othe",
+    "address",
+    "zipcode",
+    "zip_city",
+    "cb_num",
+    "borocode",
+    "boroname",
+    "cncldist",
+    "st_assem",
+    "st_senate",
+    "nta",
+    "nta_name",
+    "boro_ct",
+    "state",
+    "Latitude",
+    "longitude",
+    "x_sp",
+    "y_sp",
+  ];
+  const columnMappings: [string, string][] = [
+    // ['temp-table', 'primary-table']
+    ["tree_id", "treeId"],
+    ["the_geom", "coords"],
+    ["health", "health"],
+    ["spc_common", "species"],
+    ["problems", "problems"],
+    ["address", "address"],
+    ["zipcode", "zipcode"],
+    ["zip_city", "city"],
+    ["state", "state"],
+  ];
   using importer = new PostgresCSVImporter({
     filePath,
-    temporaryTableName: "ul_temp_trees",
-    primaryTableName: "trees",
-    propertyMappings: [
-      // ['temp-table', 'primary-table']
-      ["tree_id", "id"],
-      ["the_geom", "coords"],
-      ["health", "health"],
-      ["spc_common", "species"],
-      ["problems", "problems"],
-      ["address", "address"],
-      ["zipcode", "zipcode"],
-      ["zip_city", "city"],
-      ["state", "state"],
-    ],
+    csvHeaders,
+    columnMappings,
+    temporaryTable: {
+      name: "ul_temp_trees",
+      primaryKey: "tree_id",
+    },
+    primaryTable: {
+      name: "trees",
+      primaryKey: "id",
+    },
   });
 
+  // load dependencies
   await importer.init();
+
+  // start importing from local file to postgres
   await importer.import();
 })();
