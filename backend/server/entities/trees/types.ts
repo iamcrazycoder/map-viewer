b741c3a20105ef804823906aba8cd02d0fd3702f
Nishant Ghodke <wtfcrazycoder@gmail.com>
2023-12-14 22:03:22 +0530

    feat(backend): add types for Tree entity queries

---
+++ backend/server/entities/trees/types.ts
@@ -0,0 +1,33 @@
+import { StringToArrayOfStrings } from "../../utils/types";
+
+export interface Tree {
+  id: string;
+  treeId: string;
+  latitude: number;
+  longitude: number;
+  status: "Alive" | "Stump" | "Dead" | null;
+  health: string | null;
+  species: string;
+  problems: string[] | null;
+  address: string | null;
+  zipcode: string | null;
+  city: string | null;
+  state: string | null;
+}
+
+export type GetTreesFilterAttributes = StringToArrayOfStrings<
+  Pick<Tree, "health" | "species" | "problems">
+>;
+
+export type ParsedFilterAttributes = {
+  keys: string[];
+  values: string[][];
+};
+
+export type CoOrdinates = {
+  longitude: number;
+  latiture: number;
+};
+
+// [[x-min, y-min], [x-max, y-max]]
+export type BoundingBox = [[number, number], [number, number]];
