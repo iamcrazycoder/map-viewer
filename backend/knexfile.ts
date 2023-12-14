2b029ab1829405aeecfa2d3ef1865f6694e7fe29
Nishant Ghodke <wtfcrazycoder@gmail.com>
2023-12-14 16:54:07 +0530

    chore(backend): lint knexfile

--- backend/knexfile.ts
+++ backend/knexfile.ts
@@ -1,29 +1,27 @@
-import * as dotenv from 'dotenv'
-import type { Knex } from 'knex'
-import { knexSnakeCaseMappers } from 'objection'
+import * as dotenv from "dotenv";
+import type { Knex } from "knex";
 
-dotenv.config()
+dotenv.config();
 
 const config: { [key: string]: Knex.Config } = {
   development: {
-    client: 'pg',
+    client: "pg",
     connection: {
       host: process.env.PG_HOST,
       port: Number(process.env.PG_PORT),
       user: process.env.PG_USER,
       password: process.env.PG_PASSWORD,
-      database: process.env.PG_DATABASE
+      database: process.env.PG_DATABASE,
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations',
-      directory: './migrations'
+      tableName: "knex_migrations",
+      directory: "./migrations",
     },
-    ...knexSnakeCaseMappers()
-  }
-}
+  },
+};
 
-export default config
+export default config;
