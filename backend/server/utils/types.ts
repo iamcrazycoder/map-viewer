1c526124a20c9c451e8230c0f164d6a37a5cc0da
Nishant Ghodke <wtfcrazycoder@gmail.com>
2023-12-19 18:06:57 +0530

    refact(backend): allow null in array of strings

--- backend/server/utils/types.ts
+++ backend/server/utils/types.ts
@@ -2,6 +2,6 @@ export type StringToArrayOfStrings<T extends object> = {
   [P in keyof T as T[P] extends string | null ? P : P]-?: T[P] extends
     | string
     | null
-    ? NonNullable<T[P]>[]
+    ? T[P][]
     : NonNullable<T[P]>;
 };
