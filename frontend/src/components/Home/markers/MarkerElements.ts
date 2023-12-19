520af8ae8e0f4c4e200cc2049216531e662a2079
Nishant Ghodke <wtfcrazycoder@gmail.com>
2023-12-19 18:24:50 +0530

    feat(frontend): add base marker elems

---
+++ frontend/src/components/Home/markers/MarkerElements.ts
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const MarkerBtn = styled.button`
+  background: none;
+  border: none;
+  cursor: pointer;
+  position: relative;
+  padding: 0;
+  margin-bottom: 75px;
+  text-align: center;
+`;
+
+export const MarkerText = styled.p`
+  background: #fff;
+  padding: 2px 5px;
+  border-radius: 3px;
+  color: #000;
+  bottom: 0px;
+  right: 0px;
+  left: 0px;
+  top: 0px;
+  font-size: 9px;
+  font-weight: 800;
+  height: 10px;
+`;
+
+export const MarkerIcon = styled.img`
+  width: 22px;
+  z-index: 9999;
+`;
